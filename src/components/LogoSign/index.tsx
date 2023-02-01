7e8e752615f86b08c082ee97a50939a62da10ef4
Abderrahim Tantaoui <abdoutanta@gmail.com>
2023-02-01 17:53:53 +0100

    fix: center and adjust logo size on mobile

--- src/components/LogoSign/index.tsx
+++ src/components/LogoSign/index.tsx
@@ -14,67 +14,11 @@ const LogoWrapper = styled(Link)(
   ({ theme }) => `
         color: ${theme.palette.text.primary};
         display: flex;
+        align-items: center;
         text-decoration: none;
         width: 53px;
         margin: 0 auto;
         font-weight: ${theme.typography.fontWeightBold};
-`
-);
-
-const LogoSignWrapper = styled(Box)(
-  () => `
-        width: 52px;
-        height: 38px;
-`
-);
-
-const LogoSign = styled(Box)(
-  ({ theme }) => `
-        background: ${theme.general.reactFrameworkColor};
-        width: 18px;
-        height: 18px;
-        border-radius: ${theme.general.borderRadiusSm};
-        position: relative;
-        transform: rotate(45deg);
-        top: 3px;
-        left: 17px;
-
-        &:after, 
-        &:before {
-            content: "";
-            display: block;
-            width: 18px;
-            height: 18px;
-            position: absolute;
-            top: -1px;
-            right: -20px;
-            transform: rotate(0deg);
-            border-radius: ${theme.general.borderRadiusSm};
-        }
-
-        &:before {
-            background: ${theme.palette.primary.main};
-            right: auto;
-            left: 0;
-            top: 20px;
-        }
-
-        &:after {
-            background: ${theme.palette.secondary.main};
-        }
-`
-);
-
-const LogoSignInner = styled(Box)(
-  ({ theme }) => `
-        width: 16px;
-        height: 16px;
-        position: absolute;
-        top: 12px;
-        left: 12px;
-        z-index: 5;
-        border-radius: ${theme.general.borderRadiusSm};
-        background: ${theme.header.background};
 `
 );
 
@@ -104,13 +48,11 @@ function Logo() {
   return (
     <TooltipWrapper title="Grash" arrow>
       <LogoWrapper to="/overview">
-        <LogoSignWrapper>
           <img
             src={'/static/images/logo/logo.png'}
-            width={`${width * (mobile ? 0.5 : 1)}px`}
-            height={`${height * (mobile ? 0.5 : 1)}px`}
+            width={`${width * (mobile ? 0.7 : 1)}px`}
+            height={`${height * (mobile ? 0.7 : 1)}px`}
           />
-        </LogoSignWrapper>
       </LogoWrapper>
     </TooltipWrapper>
   );
